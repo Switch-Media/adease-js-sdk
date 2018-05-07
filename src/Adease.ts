a7771abf25a89ca3898b75493bdce25c8bda89c2
Kaley Main <kaleypoole17@gmail.com>
2018-05-07 14:53:31 +1000

    Add nyc support

--- src/Adease.ts
+++ src/Adease.ts
@@ -149,6 +149,17 @@ export default class Adease {
   }
 
   /**
+   * Notify that a player event has occured, such as resume, pause, mute or unmute.
+   * This will send the appropriate beacons for that event.
+   *
+   * @param evt The type of event, such as "mute" or "pause".
+   * @param timeMs
+   */
+  notifyPlayerEvent(evt: EventType, timeMs: number): Promise<void> {
+    return this.sendBeacons(timeMs, evt);
+  }
+
+  /**
    * Since inserting ads into a stream changes the duration, it can be useful to translate
    * between the original time of the asset and the corresponding time in the loaded stream.
    * This method returns the time in the stream that a position in the original asset corresponds to.
@@ -270,11 +281,20 @@ export default class Adease {
   /**
    * @return A promise that resolves once all beacons are sent.
    */
-  private sendBeacons(time: number): Promise<undefined> {
+  private sendBeacons(time: number, eventType?: EventType): Promise<undefined> {
+    // If eventType is provided, then send those beacons.
+    // Otherwise send the linear event beacons.
+    const kindFilter = () => {
+      if (eventType != null) {
+        return (tURL: ITrackingURL) =>
+          tURL ? (tURL.kind as EventType) === eventType : false;
+      }
+      return (tURL: ITrackingURL) =>
+        tURL ? LinearEvents.includes(tURL.kind as EventType) : false;
+    };
+
     const ps = this.getBeaconsForRange(this.lastTimePosition, time)
-      .filter(
-        tURL => (tURL ? LinearEvents.includes(tURL.kind as EventType) : false)
-      )
+      .filter(kindFilter())
       .filter(
         tURL =>
           tURL
